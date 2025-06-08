5471da2e107a58adb309fcb43812f4b397b2d5ab
danielbarnur <danielbarnur10@gmail.com>
2025-06-08 14:21:47 +0300

    feat: implement episode detail loading and UI updates

--- scripts/utils/utils.js
+++ scripts/utils/utils.js
@@ -1,4 +1,9 @@
- // Debounce utility
+export const ApiUrl = {
+  "characters": "https://rickandmortyapi.com/api/character",
+  "locations": "https://rickandmortyapi.com/api/location",
+  "episodes": "https://rickandmortyapi.com/api/episode"
+};
+// Debounce utility
 export function debounce(fn, delay) {
   let timeoutId;
   return (...args) => {
