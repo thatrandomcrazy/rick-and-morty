8e79558bc6afaa361f15da354e3c498d2724e3af
danielbarnur <danielbarnur10@gmail.com>
2025-06-11 14:59:02 +0300

    serve.js

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const app = express();
+
+app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`Listening on port:${PORT}`);
+})
